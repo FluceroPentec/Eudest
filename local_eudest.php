b238368366f6daec921ed5d235343ed17eb13b98
FluceroPentec <flucero@pentec.es>
2017-06-28 12:32:30 +0200

    inactivemessages v2

--- local_eudest.php
+++ local_eudest.php
@@ -767,8 +767,8 @@ class local_eudest {
                              GROUP BY userid
                             HAVING num_months >= 6) la
                      WHERE la.userid = u.userid
-                       AND startdate < $datetoday
-                       AND enddate > $datetoday
+                       AND startdate < NOW()
+                       AND enddate > NOW()
                        AND inactivity6 = 0";
             } else {
                 $sql = "SELECT u.*
