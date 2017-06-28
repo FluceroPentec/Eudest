7b322f63b56d5ad04da13ebb1451fc635441f177
FluceroPentec <flucero@pentec.es>
2017-06-28 12:41:55 +0200

    inactivemessages v3

--- local_eudest.php
+++ local_eudest.php
@@ -762,7 +762,7 @@ class local_eudest {
                 $sql = "SELECT u.*
                       FROM {local_eudest_masters} u,
                            (SELECT userid,
-                                    date_part('month',max(timeaccess)) - date_part('month',time())) num_months
+                                    date_part('month',max(timeaccess)) - date_part('month',time()) num_months
                               FROM {user_lastaccess}
                              GROUP BY userid
                             HAVING num_months >= 6) la
